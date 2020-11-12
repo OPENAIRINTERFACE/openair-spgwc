05b7add23a1d2b2530e9484b82873910f9c727c5
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2020-11-12 17:36:56 +0100

    [CLEANUP] file requires 2 passes of clang-format
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/common/3gpp_24.008.h
+++ src/common/3gpp_24.008.h
@@ -40,12 +40,12 @@
 
 #define INVALID_LAC_0000                                                       \
   (uint16_t) 0x0000 /*!< \brief  This LAC can be coded using a full            \
-                       hexadecimal representation except for the following                                       \
+                       hexadecimal representation except for the following     \
                        reserved hexadecimal values: 0000, and FFFE.   */
 
 #define INVALID_LAC_FFFE                                                       \
   (uint16_t) 0xFFFE /*!< \brief  This LAC can be coded using a full            \
-                       hexadecimal representation except for the following                                       \
+                       hexadecimal representation except for the following     \
                        reserved hexadecimal values: 0000, and FFFE.   */
 
 //------------------------------------------------------------------------------
